68e4217f3714725affba485243bb9ddef66c78cb
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-06-20 23:58:37 +0900

    Update 별 찍기 － 2.py

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -5,3 +5,9 @@ for i in range(1,star_rows+1):
     print(' '*j,end='')
     print('*'*i)
     j -= 1
+
+// 수정
+star_rows = int(input())
+
+for i in range(1,star_rows+1):
+    print(' '*(star_rows-i)+'*'*i)
