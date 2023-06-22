d27c59fe6967101efb4686426fe8fa44f3e9210e
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-06-22 18:13:19 +0900

    [Refactor] 숫자의 개수 2.py

--- 백준/Bronze/21567. 숫자의 개수 2/숫자의 개수 2.py
+++ 백준/Bronze/21567. 숫자의 개수 2/숫자의 개수 2.py
@@ -12,3 +12,11 @@ for i in operand:
     
 for i in number:
     print(number[i])
+
+# 입력 한 번에 받기
+# a,b,c = (int(sys.stdin.readline()) for _ in range(3))
+
+# 문자열 count 함수 써서 계산하기
+# for i in range(10):
+#     print(operand.count(str(i)))
+    
