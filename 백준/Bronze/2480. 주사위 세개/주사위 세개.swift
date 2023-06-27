aee12c729772d085552affba0e0aa89dc022183f
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-06-27 21:52:21 +0900

    [Bronze IV] Title: 주사위 세개, Time: 24 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+let array = readLine()!.components(separatedBy: " ").map { Int($0)! }
+var sameNumberCount = 0
+var sameNumber = 0
+var maxNumber = 0
+for i in 0..<3 {
+    for j in (i+1)..<3 {
+        if array[i] == array[j] {
+            sameNumberCount += 1
+            sameNumber = array[i]
+        }
+    }
+    if maxNumber < array[i] {
+        maxNumber = array[i]
+    }
+}
+
+if sameNumberCount == 0 {
+    print(maxNumber*100)
+} else if sameNumberCount == 3 {
+    print(10000+sameNumber*1000)
+} else {
+    print(1000+sameNumber*100)
+}
