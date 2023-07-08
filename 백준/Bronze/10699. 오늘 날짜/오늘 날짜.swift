28a72d2cba2ca4cffb7c67c69674e288526a73e9
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-07-08 23:52:49 +0900

    [Refactor] 오늘 날짜.swift

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.swift
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.swift
@@ -1,7 +1,6 @@
 import Foundation
 
-// var formatter = DateFormatter()
-//formatter.dateFormat = "yyyy-MM-dd"
-//var currentDate = formatter.string(from: Date())
-// print(currentDate)
-print("2023-07-08")
+var formatter = DateFormatter()
+formatter.dateFormat = "yyyy-MM-dd"
+var currentDate = formatter.string(from: Date())
+print(currentDate)
