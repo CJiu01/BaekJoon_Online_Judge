a4b498f72419020b66cd8ea7af609dd7cad10fe9
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-07-08 23:51:32 +0900

    [Bronze V] Title: 오늘 날짜, Time: 8 ms, Memory: 79500 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+// var formatter = DateFormatter()
+//formatter.dateFormat = "yyyy-MM-dd"
+//var currentDate = formatter.string(from: Date())
+// print(currentDate)
+print("2023-07-08")
