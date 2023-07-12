49978aa58dea1bf5a53edba670235c71ff8355a5
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-07-12 12:39:23 +0900

    [Bronze III] Title: 윷놀이, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.swift
+++ 백준/Bronze/2490. 윷놀이/윷놀이.swift
@@ -1,53 +1,14 @@
 import Foundation
 
-class beakjoon2490 {
-    
-    var res_arr = Array<Int>()
-    
-    func getData() {
-        
-        for _ in 1 ... 3 {
-            let input = readLine()
-            let input_division = input?.components(separatedBy: " ")
-            
-            res_arr.append(YutCheck(arr: input_division!))
-        }
-        resultPrint(arr: res_arr)
-    }
-    
-    func YutCheck(arr: Array<String>) -> Int {
-        
-        var one_count = 0
-        
-        // 배열에서 0이 몇 개인지 검사
-        for i in 0 ..< arr.count {
-            if Int(arr[i])! == 0 {
-                one_count += 1
-            }
-        }
-        
-        return one_count
-    }
-    
-    func resultPrint(arr: Array<Int>) {
-        for i in 0 ..< arr.count {
-            switch arr[i] {
-            case 0:
-                print("E")
-            case 1:
-                print("A")
-            case 2:
-                print("B")
-            case 3:
-                print("C")
-            case 4:
-                print("D")
-            default:
-            return
-            }
-        }
-    }
-}
+let score: [Int: String] = [0: "E", 1: "A", 2: "B", 3: "C", 4: "D"]
 
-let boj_2490 = beakjoon2490()
-boj_2490.getData()
+for _ in 0..<3 {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    var cnt = 0
+    for i in 0..<4 {
+        if input[i] == 0 {
+            cnt += 1
+        }
+    }
+    print(score[cnt]!)
+}
