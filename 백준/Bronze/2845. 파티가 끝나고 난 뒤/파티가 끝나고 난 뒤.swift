1f737421afc37d594ce2c019cfbd7b0475929dd5
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-07-12 11:35:42 +0900

    [Refactor] 파티가 끝나고 난 뒤.swift

--- 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.swift
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.swift
@@ -1,9 +1,16 @@
-let exactPeopleCount : Int = {
-    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-    return input[0] * input[1]
-}()
+import Foundation
 
-let result = readLine()!.split(separator: " ").map {
-    String(Int(String($0))! - exactPeopleCount)
-}.joined(separator: " ")
-print(result)
+let partyPlace = readLine()!.split(separator: " ").map { Int($0)! }
+let expectedNumber = partyPlace[0]*partyPlace[1]
+
+let articleNumber = readLine()!.split(separator: " ").map { Int($0)! }
+for i in 0..<5 {
+    print(articleNumber[i]-expectedNumber, terminator: " ")
+}
+
+// Refactor
+let partyPlace: Int = {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    return input[0]*input[1]
+}()
+print(readLine()!.split(separator: " ").map { String(Int($0)! - partyPlace) }.joined(separator: " "))
