2ddef0f2e484f97b4b7e8548dba2a3a741c1bb89
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-07-13 18:17:34 +0900

    [Refactor] 공 바꾸기.swift

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.swift
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.swift
@@ -11,3 +11,18 @@ for _ in 0..<countInput[1] {
 for i in 1..<nums.count {
     print(nums[i], terminator: " ")
 }
+
+// Refactor
+
+func solution() {
+    let countInput = readLine()!.split(separator: " ").map { Int($0)! }
+    var nums = Array(0...countInput[0])
+    
+    for _ in 0..<countInput[1] {
+        let switchNumber = readLine()!.split(separator: " ").map { Int($0)! }
+        nums.swapAt(switchNumber[0], switchNumber[1]) // swapAt() 함수 사용
+    }
+    nums.forEach { print($0, terminator: " ")}    // 반복문 대신 forEach 사용
+}
+
+solution()
