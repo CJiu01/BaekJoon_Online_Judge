cb5b77167886605999e6c0a36cb2609c23a4edc5
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2023-08-01 21:12:03 +0900

    [Refactor] 별 찍기 － 4.swift

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.swift
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.swift
@@ -13,3 +13,12 @@ func solution() {
 }
 
 solution()
+
+
+// Refactor
+
+let n = Int(readLine()!)!
+
+for i in 0..<n {
+    print(String(repeating: "", count: i) + String(repeating: "*", count: n-i))
+}
