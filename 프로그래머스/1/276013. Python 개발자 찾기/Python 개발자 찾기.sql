6c82460b46d5f5e37e449995adabd12ab7290497
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-02-25 22:31:58 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,7 +1,5 @@
 -- 코드를 작성해주세요
 select ID, EMAIL, FIRST_NAME, LAST_NAME
 from DEVELOPER_INFOS
-where skill_1 = 'Python' or
-    skill_2 = 'Python' or
-    skill_3 = 'Python' 
+where 'Python' in (skill_1, skill_2, skill_3)
 order by ID
