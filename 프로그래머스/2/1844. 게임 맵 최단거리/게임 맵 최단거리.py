aa6662db9ba9251cb606b468fb42e2ba22e74bc9
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-05-08 01:08:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.41 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,11 +2,9 @@ from collections import deque
 
 def solution(maps):
     answer = 0
-    queue = deque(())
-    queue.append((0,0))
+    queue = deque([(0, 0)])
     dir = [[0,1], [0,-1], [1,0], [-1,0]]
-    n = len(maps)
-    m = len(maps[0])
+    n,m = len(maps), len(maps[0])
     
     while queue:
         x, y = queue.popleft()
