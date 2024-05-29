8fa73f55f9b1a21bf60a7e5d54335b292e4db577
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-05-29 18:16:43 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,5 @@
-def solution(nums):
-    
+def solution(nums): 
     poncatmon_kind_num = len(set(nums))
-    catch_num = len(nums) // 2
+    catch_num = len(nums)//2
     result = min(poncatmon_kind_num, catch_num)
-
     return result
