13a50cd4d22ded8d7bd20826a1962ccf0cfd17a2
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-05-30 17:20:57 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,30 @@
-from collections import Counter
+# from collections import Counter
+
+# def solution(clothes):
+#     answer = 1
+    
+#     clothes_kind = []
+#     for item in clothes:
+#         clothes_kind.append(item[1])
+#     clothes_kind = Counter(clothes_kind)
+    
+#     for item in clothes_kind:
+#         answer *= (clothes_kind[item]+1)
+
+#     return (answer-1)
+
 
 def solution(clothes):
-    answer = 1
+    clothes_type = {}
     
-    clothes_kind = []
-    for item in clothes:
-        clothes_kind.append(item[1])
-    clothes_kind = Counter(clothes_kind)
-    
-    for item in clothes_kind:
-        answer *= (clothes_kind[item]+1)
-
-    return (answer-1)
+    for c, t in clothes:
+        if t not in clothes_type:
+            clothes_type[t] = 2
+        else:
+            clothes_type[t] += 1
+            
+    cnt = 1
+    for num in clothes_type.values():
+        cnt *= num
+        
+    return cnt - 1
