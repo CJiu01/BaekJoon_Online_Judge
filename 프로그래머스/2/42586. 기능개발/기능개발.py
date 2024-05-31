d22ffc9ae33fb10de744d63168ffa843bf6949be
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-05-31 16:15:21 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,36 +1,19 @@
-import math
-
 def solution(progresses, speeds):
     answer = []
-    released = [False]*len(progresses)
-    left = cal_completed_days(progresses, speeds)
-
-    i = 0
-    while i < (len(progresses)):
-        cnt = 0
-        if released[i] == False:
+    count = 0
+    time = 0
     
-            day = left[i]
-            released[i] = True
-            cnt += 1
-            
-            while i<len(progresses)-1:
-                if left[i+1] > day:
-                    break
-                else:
-                    released[i+1] = True
-                    cnt += 1
-                    i += 1
-        i+=1
-        print("cnt: ",cnt)
-        answer.append(cnt)
+    while len(progresses) > 0:
+        if progresses[0] + (speeds[0]*time) >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+
+            time += 1
+    answer.append(count)
 
     return answer
-
-def cal_completed_days(progresses,speeds):
-    left = []
-    
-    for i in range(len(progresses)):
-        day = 100-progresses[i]
-        left.append(math.ceil(day/speeds[i]))
-    return left
