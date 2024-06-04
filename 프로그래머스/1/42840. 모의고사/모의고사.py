62f0ba363e5738883dd5ac94bcffa579da7e8f7e
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-06-04 15:32:47 +0900

    [level 1] Title: 모의고사, Time: 2.45 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,24 @@
 def solution(answers):
-    student_1 = [1, 2, 3, 4, 5]
-    student_2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    student_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    collects = [0,0,0]
-    result = []
+    student1 = [1,2,3,4,5]
+    student2 = [2,1,2,3,2,4,2,5]
+    student3 = [3,3,1,1,2,2,4,4,5,5]
     
-    for i, answer in enumerate(answers):
-        if student_1[i % len(student_1)] == answer:
-            collects[0] += 1
-        if student_2[i % len(student_2)] == answer:
-            collects[1] += 1
-        if student_3[i % len(student_3)] == answer:
-            collects[2] += 1
-
-    for i, collect in enumerate(collects):
-        if max(collects) == collect:
-            result.append(i+1)
-    return result
+    s1, s2, s3 = 0,0,0
+    for i in range(len(answers)):
+        if answers[i] == student1[i%5]:
+            s1 += 1
+        if answers[i] == student2[i%8]:
+            s2 += 1
+        if answers[i] == student3[i%10]:
+            s3 += 1
+    
+    answers = []
+    max_value = max(s1,s2,s3)
+    if s1 == max_value:
+        answers.append(1)
+    if s2 == max_value:
+        answers.append(2)
+    if s3 == max_value:
+        answers.append(3)
+        
+    return answers
