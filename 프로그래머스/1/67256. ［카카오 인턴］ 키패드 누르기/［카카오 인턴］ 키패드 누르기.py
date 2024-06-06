bdcf8eec7932e8ff18c2155dd398f3d89af0ad82
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-06-06 16:55:35 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,44 +1,83 @@
-def moved_distance(current, target):
-    d = abs(current-target)  
-    if d%3 == 0:
-        return d//3
-    else:
-        return (d//3 + d%3)
+# def moved_distance(current, target):
+#     d = abs(current-target)  
+#     if d%3 == 0:
+#         return d//3
+#     else:
+#         return (d//3 + d%3)
+
+# def solution(numbers, hand):
+#     answer = []
+#     left_side = [1,4,7]
+#     right_side = [3,6,9]
+#     left_position = 10
+#     right_position = 12
+    
+#     for target in numbers:
+        
+#         if target == 0:
+#             target = 11
+        
+#         if target in left_side:
+#             dir = 'L'
+#         elif target in right_side:
+#             dir = 'R'
+#         else:
+#             tmp_left = moved_distance(left_position, target)
+#             tmp_right = moved_distance(right_position, target)
+#             if tmp_left > tmp_right:
+#                 dir = 'R'
+#             elif tmp_left < tmp_right:
+#                 dir = 'L'
+#             else:
+#                 if hand == 'right':
+#                     dir = 'R'
+#                 else:
+#                     dir = 'L'
+        
+#         if dir == 'L':
+#             answer.append('L')
+#             left_position = target
+#         else:
+#             answer.append('R')
+#             right_position = target
+            
+#     return ''.join(answer)
 
 def solution(numbers, hand):
-    answer = []
-    left_side = [1,4,7]
-    right_side = [3,6,9]
-    left_position = 10
-    right_position = 12
-    
-    for target in numbers:
-        
-        if target == 0:
-            target = 11
-        
-        if target in left_side:
-            dir = 'L'
-        elif target in right_side:
-            dir = 'R'
+    answer = ''
+    key_dict = {1:(0,0),2:(0,1),3:(0,2),
+                4:(1,0),5:(1,1),6:(1,2),
+                7:(2,0),8:(2,1),9:(2,2),
+                '*':(3,0),0:(3,1),'#':(3,2)}
+    left = [1,4,7]
+    right = [3,6,9]
+    lhand = '*'
+    rhand = '#'
+    for i in numbers:
+        if i in left:
+            answer += 'L'
+            lhand = i
+        elif i in right:
+            answer += 'R'
+            rhand = i
         else:
-            tmp_left = moved_distance(left_position, target)
-            tmp_right = moved_distance(right_position, target)
-            if tmp_left > tmp_right:
-                dir = 'R'
-            elif tmp_left < tmp_right:
-                dir = 'L'
-            else:
-                if hand == 'right':
-                    dir = 'R'
-                else:
-                    dir = 'L'
-        
-        if dir == 'L':
-            answer.append('L')
-            left_position = target
-        else:
-            answer.append('R')
-            right_position = target
+            curPos = key_dict[i]
+            lPos = key_dict[lhand]
+            rPos = key_dict[rhand]
+            ldist = abs(curPos[0]-lPos[0]) + abs(curPos[1]-lPos[1])
+            rdist = abs(curPos[0]-rPos[0]) + abs(curPos[1]-rPos[1])
             
-    return ''.join(answer)
+            if ldist < rdist:
+                answer += 'L'
+                lhand = i
+            elif ldist > rdist:
+                answer += 'R'
+                rhand = i
+            else:
+                if hand == 'left':
+                    answer += 'L'
+                    lhand = i
+                else:
+                    answer += 'R'
+                    rhand = i
+    return answer
