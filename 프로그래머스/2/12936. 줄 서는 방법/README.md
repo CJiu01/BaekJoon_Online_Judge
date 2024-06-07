8865b4fc6be1c8e65c194aaf92075c16f367cff4
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-06-07 19:01:15 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/README.md
+++ 프로그래머스/2/12936. 줄 서는 방법/README.md
@@ -1,6 +1,6 @@
 # [level 2] 줄 서는 방법 - 12936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 07일 19:00:34
+2024년 06월 07일 19:01:14
 
 ### 문제 설명
 
--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,15 +1,4 @@
-from math import factorial
+from itertools import permutations
 
 def solution(n, k):
-    
-    arr = [i for i in range(1,n+1)]
-    answer = []
-    
-    while arr:
-        a = (k-1) // factorial(n-1)
-        answer.append(arr.pop(a))
-        
-        k = k%factorial(n-1)
-        n -= 1
-        
-    return answer
+    return list(permutations(range(1,n+1),n))[k-1]
