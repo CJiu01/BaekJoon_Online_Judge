9f5a2954d30975a4c6ed1ca1604d0b7b7ea8e3bf
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-06-07 16:47:37 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,17 +1,32 @@
+# def solution(skill, skill_trees):
+#     answer = 0
+    
+#     for s in skill_trees:
+#         idx = [100] * len(skill)
+        
+#         for i in range(len(skill)):
+#             try:
+#                 idx[i] = s.index(skill[i])
+#             except ValueError:
+#                 idx[i] = 100
+
+#         idx_sorted = sorted(idx)
+#         if idx == idx_sorted:
+#             answer += 1
+            
+#     return answer
+
 def solution(skill, skill_trees):
     answer = 0
     
-    for s in skill_trees:
-        idx = [100] * len(skill)
+    for skills in skill_trees:
+        skill_list = list(skill)
         
-        for i in range(len(skill)):
-            try:
-                idx[i] = s.index(skill[i])
-            except ValueError:
-                idx[i] = 100
-
-        idx_sorted = sorted(idx)
-        if idx == idx_sorted:
+        for s in skills:
+            if s in skill:
+                if s != skill_list.pop(0):
+                    break
+        else:
             answer += 1
             
     return answer
