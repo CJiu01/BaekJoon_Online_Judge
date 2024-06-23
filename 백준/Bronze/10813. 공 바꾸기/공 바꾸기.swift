a69e42f501df7070cab25de1cdefd32ddc38e33d
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-06-23 23:32:42 +0900

    [Bronze II] Title: 공 바꾸기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.swift
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.swift
@@ -1,28 +1,14 @@
-let countInput = readLine()!.split(separator: " ").map { Int($0)! }
-var nums = Array(0...countInput[0])
-for _ in 0..<countInput[1] {
-    var tmp = 0
-    let switchNumber = readLine()!.split(separator: " ").map { Int($0)! }
-    tmp = nums[switchNumber[0]]
-    nums[switchNumber[0]] = nums[switchNumber[1]]
-    nums[switchNumber[1]] = tmp
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let (N,M) = (input[0],input[1])
+
+var basket = Array<Int>(0...N)
+
+
+for i in 0..<M {
+    let ball = readLine()!.split(separator: " ").map { Int($0)! }
+    let tmp = basket[ball[0]]
+    basket[ball[0]] = basket[ball[1]]
+    basket[ball[1]] = tmp
 }
 
-for i in 1..<nums.count {
-    print(nums[i], terminator: " ")
-}
-
-// Refactor
-
-func solution() {
-    let countInput = readLine()!.split(separator: " ").map { Int($0)! }
-    var nums = Array(0...countInput[0])
-    
-    for _ in 0..<countInput[1] {
-        let switchNumber = readLine()!.split(separator: " ").map { Int($0)! }
-        nums.swapAt(switchNumber[0], switchNumber[1]) // swapAt() 함수 사용
-    }
-    nums.forEach { print($0, terminator: " ")}    // 반복문 대신 forEach 사용
-}
-
-solution()
+print(basket[1...].map { String($0) }.joined(separator: " "))
