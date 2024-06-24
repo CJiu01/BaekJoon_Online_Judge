8b07f9f9745c1b104fd5910a59e679e51c0530c7
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-06-24 19:29:07 +0900

    [Bronze II] Title: 공 바꾸기, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.swift
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.swift
@@ -3,12 +3,9 @@ let (N,M) = (input[0],input[1])
 
 var basket = Array<Int>(0...N)
 
-
 for i in 0..<M {
     let ball = readLine()!.split(separator: " ").map { Int($0)! }
-    let tmp = basket[ball[0]]
-    basket[ball[0]] = basket[ball[1]]
-    basket[ball[1]] = tmp
+    basket.swapAt(ball[0], ball[1])
 }
 
 print(basket[1...].map { String($0) }.joined(separator: " "))
