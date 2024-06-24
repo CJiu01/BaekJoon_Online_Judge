8873ee97021086a975fddc3a032656932ef603bb
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-06-24 19:57:35 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.swift
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.swift
@@ -1,19 +1,7 @@
-import Foundation
+var arr = Array(1...30)
 
-// 5597
-
-// Set 생성 및 선언
-var integerSet: Set<Int> = Set<Int>()
-var userSet: Set<Int> = Set<Int>()
-
-for i in 1..<31 {
-    integerSet.insert(i)
+for _ in 0..<28 {
+    let i = arr.firstIndex(of: Int(readLine()!)!)!
+    arr.remove(at: i)
 }
-
-for i in 0..<28 {
-    userSet.insert(Int(readLine()!)!)
-}
-let subSet: Set<Int> = integerSet.subtracting(userSet)
-for i in subSet.reversed() {
-    print(i)
-}
+arr.forEach { print($0) }
