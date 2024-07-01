afd3c2c69faf30e3697fb44961798881eaee1557
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-07-01 17:42:12 +0900

    [level 1] Title: 최소직사각형, Time: 1.65 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
@@ -1,19 +1,13 @@
-import Foundation
-
 func solution(_ sizes:[[Int]]) -> Int {
-    var sizes = sizes
-    for i in 0..<sizes.count {
-        sizes[i].sort(by: >)
+    var row = 0
+    var col = 0
+    for i in sizes {
+        var (a,b) = (i[0],i[1])
+        if a<b {
+            (a,b) = (b,a)
+        }
+        row = max(row,a)
+        col = max(col,b)
     }
-    var a = 0
-    var b = 0
-    for i in 0..<sizes.count {
-        if sizes[i][0]>a {
-            a = sizes[i][0]
-        }
-        if sizes[i][1]>b {
-            b = sizes[i][1]
-        }
-    }    
-    return a*b
+    return row*col
 }
