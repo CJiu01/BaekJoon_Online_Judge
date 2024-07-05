142cce7adb5f815cedd9b1c5f1bba29aad103b74
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-07-05 23:18:35 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,18 +1,13 @@
 import Foundation
 
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
-    var w = 0, h = 0
-    let sum = brown + yellow
     
-    for i in 1...sum {
-        if sum % i == 0 {
-            w = sum / i
-            h = i
-        }
-                
-        if (w-2) * (h-2) == yellow {
-            break
+    for i in 1...Int(sqrt(Double(yellow))) {
+        if yellow%i == 0 {
+            if ((i+2)+(yellow/i))*2 == brown {
+                return [yellow/i+2, i+2]
+            }
         }
     }
-    return [w, h]
+    return []
 }
