d58a076fb4fc5241ca72a28a088cf91fba33a26f
CJiu01 <102797359+CJiu01@users.noreply.github.com>
2024-10-31 14:58:48 +0900

    [Silver V] Title: 단어 정렬, Time: 316 ms, Memory: 25796 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,25 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.TreeSet;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	static int n;
+	static TreeSet<String> ts = new TreeSet<String>(new Comparator<String>() {
+		@Override
+		public int compare(String o1, String o2) {
+			//음수면 유지(o1 가 o2 보다 작으면)
+			if(o1.length() == o2.length()) {
+				return o1.compareTo(o2);
+			} else {
+				return o1.length() - o2.length();
+			}
+		}
 
-        int n = Integer.parseInt(br.readLine());
+	});
 
-        Set<String> set = new HashSet<>();
-        for(int i=0; i<n; i++) {
-            set.add(br.readLine());
-        }
-        String[] arr = set.toArray(new String[0]);
+	public static void main(String[] args) throws IOException {
+		input();
+		print();
+	}
 
-        Arrays.sort(arr, Comparator.comparing(String::length).thenComparing(String::compareTo));
+	public static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
 
-        for (String s : arr) {
-            sb.append(s).append("\n");
-        }
-        System.out.println(sb);
-    }
+		// 중복제거를 위해 set 사용
+		for(int i=0; i<n; i++) {
+			String word = br.readLine();
+			ts.add(word);
+		}
+	}
+
+	public static void print() {
+		StringBuilder sb = new StringBuilder();
+		while (!ts.isEmpty()) {
+			sb.append(ts.pollFirst()).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
