bb6b05611192e68dc5e05883dfa47dc4a61495a0
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-01-13 15:06:47 +0900

    [Silver IV] Title: 수 찾기, Time: 484 ms, Memory: 48608 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,26 +1,26 @@
-import sys
+n = int(input())
+num = list(map(int, input().split()))
+num.sort()
 
-N = int(input())
-arrlist = list(map(int,sys.stdin.readline().split()))
-M = int(input())
-solutionlist = list(map(int,sys.stdin.readline().split()))
-arrlist.sort()
+m = int(input())
+target = list(map(int, input().split()))
 
-def binarySearch(target):
+
+def binarysearch(i):
     start = 0
-    end = N-1
-
-    while(start <= end):
-        mid = (start+end) // 2
-
-        if(arrlist[mid] == target):
-            return 1
-        elif(arrlist[mid] > target):
-            end = mid-1
-        elif(arrlist[mid] < target):
-            start = mid+1
+    end = n-1
     
+    while(start <= end):
+        mid = (start+end) //2
+        
+        if(num[mid] == i):
+            return 1
+        elif(num[mid] > i):
+            end = mid-1
+        elif(num[mid] < i):
+            start = mid+1
+            
     return 0
 
-for i in range(M):
-    print(binarySearch(solutionlist[i]))
+for i in range(m):
+    print(binarysearch(target[i]))
