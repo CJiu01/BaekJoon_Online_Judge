309b772064796adfa5daff73abe6984793aa52a9
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-04-14 16:55:31 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 952 ms, Memory: 159008 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -3,54 +3,25 @@ import java.util.*;
 
 public class Main {
 
-    public static int bisectLeft(int[] list, int target) {
-        int low=0, high=list.length;
-        while(low<high) {
-            int mid = (low+high)/2;
-            if(list[mid] < target) {
-                low = mid+1;
-            } else {
-                high = mid;
-            }
-        }
-        return low;
-    }
-
-    public static int bisectRight(int[] list, int target) {
-        int low=0, high=list.length;
-        while(low<high) {
-            int mid=(low+high)/2;
-            if(list[mid]<=target) {
-                low = mid+1;
-            } else {
-                high = mid;
-            }
-        }
-        return low;
-    }
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-        int[] card = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        HashMap<String, Integer> map = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++) {
-            int num = Integer.parseInt(st.nextToken());
-            card[i] = num;
+            String s = st.nextToken();
+            map.put(s, map.getOrDefault(s, 0)+1);
         }
-        Arrays.sort(card);
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()) {
-            int num = Integer.parseInt(st.nextToken());
-            bw.write(Integer.toString(bisectRight(card, num)-bisectLeft(card, num)));
-            bw.write(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<m; i++) {
+            sb.append(map.getOrDefault(st.nextToken(), 0)+ " ");
         }
-        br.close();
-        bw.close();
+        System.out.println(sb);
     }
 }
