5e4d277bb1b28ebae2699cd6710950ffc2bfbfaf
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-06-17 23:41:33 +0900

    [Bronze II] Title: 나누기, Time: 108 ms, Memory: 14476 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int F = Integer.parseInt(br.readLine());
+		
+		N -= (N % 100);
+		
+		while(true) {
+			if(N % F == 0) {
+				break;
+			}
+			N++;
+		}
+		System.out.format("%02d", (N % 100));
+	}
+}
