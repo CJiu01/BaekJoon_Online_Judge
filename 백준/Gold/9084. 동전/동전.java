ed1f43aa08218d88ce41028055c2ad7d843e54e2
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-06-19 19:51:07 +0900

    [Gold V] Title: 동전, Time: 108 ms, Memory: 14528 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while(T-->0) {
+            int N = Integer.parseInt(br.readLine());
+            int[] coin = new int[N];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++) {
+                coin[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int M = Integer.parseInt(br.readLine());
+            int[] dp = new int[M+1];
+            dp[0] = 1;
+
+            for(int i=0; i<coin.length; i++) {
+                for(int j=coin[i]; j<=M; j++) {
+                    dp[j] += dp[j-coin[i]];
+                }
+            }
+            sb.append(dp[M]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
