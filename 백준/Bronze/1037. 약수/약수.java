69001a64a1ddcf8fb1bf8a32c46f71e22c71028f
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-06-19 20:02:07 +0900

    [Bronze I] Title: 약수, Time: 100 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -3,19 +3,18 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        int N = Integer.parseInt(br.readLine());
+        int[] divisor = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            min = Math.min(min, arr[i]);
-            max = Math.max(max, arr[i]);
+        int min = Integer.MAX_VALUE;
+        while(st.hasMoreTokens()) {
+            int tmp = Integer.parseInt(st.nextToken());
+            max = Integer.max(max, tmp);
+            min = Integer.min(min, tmp);
         }
-        System.out.print(min * max);
-        
+        System.out.println(min*max);
     }
 }
