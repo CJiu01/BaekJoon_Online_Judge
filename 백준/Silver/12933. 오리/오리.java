c63e6231c1cfbe21ad6208621bf5751c09a0deb6
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-06-28 02:07:15 +0900

    [Silver II] Title: 오리, Time: 120 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.java
+++ 백준/Silver/12933. 오리/오리.java
@@ -2,82 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static char[] duck;
 
-
-    public static int countDuck(int front, int rear) {
-
-        int count = 1;
-        for(int i=front+1; i<rear; i++) {
-            if(duck[i] == 'q') {
-                count+=1;
-            }
-        }
-        return count;
-    }
-
-    public static boolean isCorrect() {
-
-        String quack = "uack";
-        List<Character> list = new ArrayList<>();
-
-        for (char c : duck) {
-            list.add(c);
-        }
-
-        while(list.indexOf('q') != -1) {
-            int start = list.indexOf('q');
-            list.remove(start);
-
-            int check = 0;
-            for(int j=start; j<list.size(); j++) {
-                if(list.get(j) == quack.charAt(check)) {
-                    check++;
-                    list.remove(j--);
-                }
-                if(check == 4) break;
-            }
-            if(check<4) return false;
-        }
-
-        return list.isEmpty() ? true : false;
-    }
+    private static final char[] QUACK = {'q','u','a','c','k'};
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        duck = s.toCharArray();
+        char[] arr = br.readLine().toCharArray();
 
-        int res = 0;
-        int idx = 0;
-        int last = 0;
+        int remain = arr.length;
+        int ducks = 0;
 
-        if(!isCorrect()) {
-            System.out.println(-1);
-            return;
-        }
-        while(idx < duck.length) {
+        while(remain > 0) {
+            int quackIdx = 0;
+            boolean found = false;
+            int[] positions = new int[5];
 
-            if(duck[idx] == 'q') {
+            for(int i=0; i<arr.length; i++) {
 
-                // q와 가장 인접한 k를 찾기 위한 반복문
-                for(int i=idx+1; i<duck.length; i++) {
-                    if(duck[i]=='k' && i>last) {
-                        int tmp = countDuck(idx, i);
-                        if (tmp == -1) {
-                            System.out.println(-1);
-                            return;
+                if(arr[i] == QUACK[quackIdx]) {
+                    positions[quackIdx] = i;
+                    quackIdx++;
+
+                    if(quackIdx == 5) {
+                        found = true;
+                        quackIdx = 0;
+                        
+                        remain-=5;
+                        for (int k=0; k<5; k++) {
+                            arr[positions[k]] = 0;
                         }
-                        res = Math.max(res, tmp);
-                        last = i;
-                        break;
                     }
                 }
-
             }
-            idx++;
+            if(!found) {
+                System.out.println(-1);
+                return;
+            }
+            ducks++;
         }
-        System.out.println(res>0 ? res : -1);
+        System.out.println(ducks);
     }
 }
