e03442b121740c2b4a7737e4e34d02f7f82f3c87
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-07-03 19:48:09 +0900

    [Gold V] Title: ZOAC, Time: 108 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Gold/16719. ZOAC/ZOAC.java
+++ 백준/Gold/16719. ZOAC/ZOAC.java
@@ -4,46 +4,31 @@ import java.util.*;
 public class Main {
     static List<Character> word = new ArrayList<>();
     static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
 
-    // 전달받은 리스트에서 가장 앞 문자의 인덱스 반환
-    public static void find(int idx) {
-        if(idx>=word.size() || checkVisitedAll(idx)) {
-            return;
-        }
+    static void find(int left, int right) {
 
-        int front = idx;
-        for(int i=idx+1; i<word.size(); i++) {
-            if(word.get(front) > word.get(i) && !visited[i]) {
-                front = i;
+        if(left>right) return;
+
+        int idx = left;
+        for(int i=left; i<=right; i++) {
+            if(word.get(idx) > word.get(i) && !visited[i]) {
+                idx = i;
             }
         }
-        visited[front] = true;
+        visited[idx] = true;
 
         for(int i=0; i<word.size(); i++) {
-            if(visited[i]) {
-                System.out.print(word.get(i));
-            }
+            if(visited[i]) sb.append(word.get(i));
         }
-        System.out.println();
+        sb.append("\n");
 
-        int i=front+1;
-        while(!checkVisitedAll(i)) {
-            if(!visited[i]) {
-                find(i);
+        find(idx+1, right);
+        find(left, idx-1);
 
-            }
-        }
         return;
     }
 
-    static boolean checkVisitedAll(int idx) {
-        for(int i=idx; i<word.size(); i++) {
-            if(!visited[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
 
     public static void main(String[] args) throws IOException {
 
@@ -53,8 +38,8 @@ public class Main {
             word.add(c);
         }
         visited = new boolean[word.size()];
-        while (!checkVisitedAll(0)) {
-            find(0);    
-        }
+        find(0,word.size()-1);
+
+        System.out.println(sb);
     }
 }
