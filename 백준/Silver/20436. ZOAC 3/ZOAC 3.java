10f4edb97d671a34dbaaf32d6bb99912231347a1
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-07-03 16:29:38 +0900

    [Silver IV] Title: ZOAC 3, Time: 108 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Silver/20436. ZOAC 3/ZOAC 3.java
+++ 백준/Silver/20436. ZOAC 3/ZOAC 3.java
@@ -2,97 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<List<String>> keyboardLeft = new ArrayList<>();
-    static List<List<String>> keyboardRight = new ArrayList<>();
 
-    static int[] left = new int[2];
-    static int[] right = new int[2];
+    static Map<Character, int []> map1 = new HashMap<>();
+    static Map<Character, int []> map2 = new HashMap<>();
 
-        
-    static void set(String leftFirst, String rightFirst) {
-        keyboardLeft.add(Arrays.asList("q","w","e","r","t"));
-        keyboardLeft.add(Arrays.asList("a","s","d","f","g"));
-        keyboardLeft.add(Arrays.asList("z","x","c","v"));
+    static void set() {
+        map1.put('q', new int[] {0,0});
+        map1.put('w', new int[] {0,1});
+        map1.put('e', new int[] {0,2});
+        map1.put('r', new int[] {0,3});
+        map1.put('t', new int[] {0,4});
+        map1.put('a', new int[] {1,0});
+        map1.put('s', new int[] {1,1});
+        map1.put('d', new int[] {1,2});
+        map1.put('f', new int[] {1,3});
+        map1.put('g', new int[] {1,4});
+        map1.put('z', new int[] {2,0});
+        map1.put('x', new int[] {2,1});
+        map1.put('c', new int[] {2,2});
+        map1.put('v', new int[] {2,3});
 
-        keyboardRight.add(Arrays.asList(" ","y","u","i","o","p"));
-        keyboardRight.add(Arrays.asList(" ","h","j","k","l"));
-        keyboardRight.add(Arrays.asList("b","n","m"));
+        map2.put('y', new int[] {0,5});
+        map2.put('u', new int[] {0,6});
+        map2.put('i', new int[] {0,7});
+        map2.put('o', new int[] {0,8});
+        map2.put('p', new int[] {0,9});
+        map2.put('h', new int[] {1,5});
+        map2.put('j', new int[] {1,6});
+        map2.put('k', new int[] {1,7});
+        map2.put('l', new int[] {1,8});
+        map2.put('b', new int[] {2,4});
+        map2.put('n', new int[] {2,5});
+        map2.put('m', new int[] {2,6});
 
-        for(int i=0; i<3; i++) {
-            if(keyboardLeft.get(i).contains(leftFirst)) {
-                left[0] = i;
-                left[1] = keyboardLeft.get(i).indexOf(leftFirst);
-                break;
-            }
-
-        }
-
-        for(int i=0; i<3; i++) {
-            if(keyboardRight.get(i).contains(rightFirst)) {
-                right[0] = i;
-                right[1] = keyboardRight.get(i).indexOf(rightFirst);
-                break;
-            }
-        }
-    }
-
-    static int findDir(String c) {
-
-        int row = -1;
-        int col = -1;
-
-        for(int i=0; i<3; i++) {
-            if(keyboardLeft.get(i).contains(c)) {
-                row = i;
-                col = keyboardLeft.get(i).indexOf(c);
-                break;
-            }
-        }
-        
-        int dist = 0;
-        if(row == -1) {
-            for(int i=0; i<3; i++) {
-                if(keyboardRight.get(i).contains(c)) {
-                    row = i;
-                    col = keyboardRight.get(i).indexOf(c);
-                    break;
-                }
-            }
-            dist = moveRight(row, col);
-        } else {
-            dist = moveLeft(row, col);
-        }
-
-        return dist;
-
-    }
-
-    static int moveLeft(int row, int col) {
-        int dist = Math.abs(left[0]-row) + Math.abs(left[1]-col);
-        left[0] = row;
-        left[1] = col;
-        return dist;
-    }
-
-    static int moveRight(int row, int col) {
-        int dist = Math.abs(right[0]-row) + Math.abs(right[1]-col);
-        right[0] = row;
-        right[1] = col;
-        return dist;
     }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        set();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String leftFirst = st.nextToken();
-        String rightFirst = st.nextToken();
+
+        int[] left = map1.get(st.nextToken().charAt(0));
+        int[] right = map2.get(st.nextToken().charAt(0));
         String target = br.readLine();
 
-        set(leftFirst, rightFirst);
         int time = 0;
         for(char c: target.toCharArray()) {
-            time += (findDir(String.valueOf(c))+1);
+            int[] arr = new int[2];
+            if(map1.containsKey(c)) {
+                arr = map1.get(c);
+                time += Math.abs(arr[0] - left[0]) + Math.abs(arr[1] - left[1]);
+                left = arr;
+            } else {
+                arr = map2.get(c);
+                time += Math.abs(arr[0] - right[0]) + Math.abs(arr[1] - right[1]);
+                right = arr;
+            }
+            time+=1;
         }
         System.out.println(time);
     }
