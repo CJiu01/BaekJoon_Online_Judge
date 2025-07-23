d8f389f0f66a87f8737027b6ba785dcb9a71c658
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-07-24 03:08:45 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2736 ms, Memory: 448256 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -3,8 +3,6 @@ import java.util.*;
 
 public class Main {
 
-    static Map<Integer, Integer> map;
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,11 +10,8 @@ public class Main {
         int t = Integer.parseInt(br.readLine());
 
         while(t-- > 0) {
-            int k = Integer.parseInt(br.readLine());
-
-            Queue<Integer> ascendingPq = new PriorityQueue<>();
-            Queue<Integer> decendingPq = new PriorityQueue<>(Collections.reverseOrder());   
-            map = new HashMap<>();
+            int k = Integer.parseInt(br.readLine());     
+            TreeMap<Integer, Integer> map = new TreeMap<>();
 
             while(k-- > 0) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
@@ -24,46 +19,30 @@ public class Main {
                 int n = Integer.parseInt(st.nextToken());
                 switch (command) {
                     case "I":
-                        ascendingPq.offer(n);
-                        decendingPq.offer(n);
                         map.put(n, map.getOrDefault(n, 0)+1);
                         break;
                     case "D":
                         if(!map.isEmpty()) {
+                            int num;
                             if(n==1) {
-                                delete(decendingPq);
+                                num = map.lastKey();
                             } else {
-                                delete(ascendingPq);
+                                num = map.firstKey();
+                            }
+                            map.put(num, map.get(num)-1);
+                            if(map.get(num)==0) {
+                                map.remove(num);
                             }
                         }
-
                         break;
                 }
             }
             if(map.isEmpty()) {
-                sb.append("EMPTY").append("\n");
+                sb.append("EMPTY\n");
             } else {
-                int res = delete(decendingPq);
-                sb.append(res + " ");
-                if(!map.isEmpty()) res = delete(ascendingPq);
-                sb.append(res).append("\n");
+                sb.append(map.lastKey() + " " + map.firstKey()).append("\n");
             }
         }
-
         System.out.println(sb);
-    }
-
-    static int delete(Queue<Integer> q) {
-        int n;
-        while(true) {
-            n = q.poll();
-
-            int value = map.getOrDefault(n, 0);
-            if(value==0) continue;
-            else if(value==1) map.remove(n);
-            else map.put(n, value-1);
-            break;
-        }
-        return n;
     }
 }
