5ec19e3e113ded2e5dfcb7e3c39f44a78bfea0f6
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2025-08-05 18:11:34 +0900

    [Silver III] Title: 후위 표기식2, Time: 108 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -2,102 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int[] value;
-
-    static double plus(double a, double b) {
-        return a+b;
-    }
-    static double minus(double a, double b) {
-        return b-a;
-    }
-    static double mul(double a, double b) {
-        return a*b;
-    }
-    static double divide(double a, double b) {
-        return b/a;
-    }
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String input = br.readLine();
 
-        value = new int[n];
+        double[] value = new double[n];
+
+        for(int i=0; i<n; i++) {
+            value[i] = Double.parseDouble(br.readLine());
+        }
+
         Stack<Double> stack = new Stack<>();
-        int i=0;
-
-
-        while(i<n) {
-            value[i++] = Integer.parseInt(br.readLine());
-        }
-
-        i=0;
-
-        int[] convertedInput = new int[input.length()];
-
-        while(i<input.length()) {
-            char c = input.charAt(i);
-
-            if(c-'1' >= 0) {
-                convertedInput[i] = value[c-'A'];
+        for(char c: input.toCharArray()) {
+            if('A'<= c && c <= 'Z') {
+                double d = value[c-'A'];
+                stack.push(d);
             } else {
-                switch(c) {
-                    case '+':
-                        convertedInput[i] = -1;
-                        break;
-            
-                    case '-':
-                        convertedInput[i] = -2;
-                        break;
+                double b = stack.pop();
+                double a = stack.pop();
+                double res = 0;
 
-                    case '*':
-                        convertedInput[i] = -3;
-                        break;
-
-                    case '/':
-                        convertedInput[i] = -4;
-                        break;
+                switch (c) {
+                    case '+': res = a+b; break;
+                    case '-': res = a-b; break;
+                    case '*': res = a*b; break;
+                    case '/': res = a/b; break;                
                 }
+                stack.push(res);
             }
-            i++;
         }
-
-        i=0;
-        while(i<convertedInput.length) {
-            int tar = convertedInput[i++];
-
-            if(tar >= 0) {
-                stack.push(Double.valueOf(tar));
-                continue;
-            }
-         
-
-            double res = 0;
-            switch (tar) {
-                case -1:
-                    res = plus(stack.pop(), stack.pop());
-                    stack.push(res);
-                    break;
-            
-                case -2:
-                    res = minus(stack.pop(), stack.pop());
-                    stack.push(res);         
-                    break;
-
-                case -3:
-                    res = mul(stack.pop(), stack.pop());
-                    stack.push(res);   
-                    break;
-
-                case -4:
-                    res = divide(stack.pop(), stack.pop());
-                    stack.push(res);    
-                    break;
-            }
-            
-        }
-        System.out.printf("%.2f\n",stack.pop());
-
+        System.out.printf("%.2f",stack.pop());
     }
 }
