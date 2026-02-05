11d35a30f9258670ab68a89274ae6082f9b50c23
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-02-05 22:24:16 +0900

    [Bronze II] Title: 거스름돈, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,12 +1,9 @@
-money = [500, 100, 50, 10, 5, 1]
-
-user = int(input())
-user_change = 1000-user
+n = 1000 - int(input())
 count = 0
 
-for i in money:
-    count += user_change//i
-    user_change %= i
+array = [500, 100, 50, 10, 5, 1]
+for coin in array:
+    count += (n//coin)
+    n %= coin
     
-        
 print(count)
