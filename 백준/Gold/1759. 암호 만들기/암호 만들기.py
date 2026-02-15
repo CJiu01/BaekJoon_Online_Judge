ca0887fc48b1ce1e6443a9028d17a31c24504e14
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-02-15 20:48:00 +0900

    [Gold V] Title: 암호 만들기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,26 +1,13 @@
 import sys
 input = sys.stdin.readline
-
-def isValid(list):
-    # 0<vo<=l-2
-    vowel = {'a', 'e', 'i', 'o', 'u'}
-    cnt = len(set(list) & vowel)
-    if (0<cnt and cnt<=l-2):
-        return True
-    else:
-        return False
-
-def dfs(arr, cur):
-    if(len(arr)==l):
-        if (isValid(arr)):
-            print(''.join(arr))
-        return
-
-    for i in range(cur,c):
-        dfs(arr+[al[i]], i+1)
+from itertools import combinations
 
 l, c = map(int, input().split())
 al = list(input().split())
 list.sort(al)
+vowel = {'a', 'e', 'i', 'o', 'u'}
 
-dfs([], 0)
+for password in combinations(al, l):
+    cnt = len(set(password) & vowel)
+    if (0<cnt and cnt<=l-2):
+        print(''.join(password))
