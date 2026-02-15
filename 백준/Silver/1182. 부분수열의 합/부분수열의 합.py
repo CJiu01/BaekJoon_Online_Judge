79207e3d407cf93e67f01106aa8e26709887f4bb
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-02-15 18:34:11 +0900

    [Silver II] Title: 부분수열의 합, Time: 168 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,17 +1,17 @@
-def bfs(cur, total):
+def dfs(cur, total):
     global cnt
     if (cur==n):
         if(total==s): cnt+=1
         return
         
-    bfs(cur+1, total)
-    bfs(cur+1, total+arr[cur])
+    dfs(cur+1, total)
+    dfs(cur+1, total+arr[cur])
     
 
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 cnt = 0
-bfs(0,0)
+dfs(0,0)
 
 if (s==0): cnt-=1
 print(cnt)
