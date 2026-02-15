022e41816e761ae294860ae32dae1d449e91aa7d
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-02-15 12:01:39 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,11 @@
+def back(arr, idx):
+    if (len(arr)==M):
+        print(*arr)
+        return
+    
+    for i in range(idx,N+1):
+        back(arr+[i], i)
+        
+N, M = map(int, input().split())
+arr = []
+back(arr, 1)
