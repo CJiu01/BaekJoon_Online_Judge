76fb0e69fde21c6b484e97cb028758ff888005f0
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-02-18 23:21:17 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
@@ -1,30 +1,10 @@
-from collections import deque
-
-def simul(row, col, move):
-    if (move==0):
-        row.insert(0, row.pop())
-        col[1] = row[1]
-        col[3] = row[3]
-    elif (move==1):
-        row.append(row.popleft())
-        col[1] = row[1]
-        col[3] = row[3]
-    elif (move==2):
-        col.append(col.popleft())
-        row[1] = col[1]
-        row[3] = col[3]
-    elif (move==3):
-        col.insert(0, col.pop())
-        row[1] = col[1]
-        row[3] = col[3]
-
 N, M, x, y, K = map(int,input().split())  
 arr = [list(map(int, input().split())) for _ in range(N)]
 moves = list(map(int, input().split()))
 
 dir = [[0,1],[0,-1],[-1,0],[1,0]]
-row = deque([0]*4)
-col = deque([0]*4)
+dice = [0]*6
+
 for move in moves:
     dx = x+dir[move-1][0]
     dy = y+dir[move-1][1]
@@ -33,12 +13,20 @@ for move in moves:
         continue
     
     x, y = dx, dy 
-    simul(row, col, move-1)
+    d0, d1, d2, d3, d4, d5= dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
+    if (move==1):
+        dice[1], dice[3], dice[4], dice[5] = d4, d5, d3, d1
+    elif (move==2):
+        dice[1], dice[3], dice[4], dice[5] = d5, d4, d1, d3
+    elif (move==3):
+        dice[0], dice[1], dice[2], dice[3] = d1, d2, d3, d0
+    elif (move==4):
+        dice[0], dice[1], dice[2], dice[3] = d3, d0, d1, d2
+        
     if (arr[x][y] == 0):
-        arr[x][y] = row[3]
+        arr[x][y] = dice[3]
     else:
-        row[3] = arr[x][y]
-        col[3] = arr[x][y]
+        dice[3] = arr[x][y]
         arr[x][y] = 0
 
-    print(row[1])
+    print(dice[1])
