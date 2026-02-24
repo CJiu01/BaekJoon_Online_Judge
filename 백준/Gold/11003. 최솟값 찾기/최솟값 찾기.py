a941dc2a4d6bfad9be148bc80511ecd069168499
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-02-24 15:19:20 +0900

    [Gold I] Title: 최솟값 찾기, Time: 6140 ms, Memory: 624208 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
@@ -5,18 +5,16 @@ from collections import deque
 N, L = map(int, input().split())
 window = list(map(int, input().split()))
 D = [0]*N
-h = deque([(window[0],0)]) # value, idx
+h = deque([0]) 
 D[0] = window[0]
 
 for i in range(1,N):
-    while (h and h[0][1] < (i-L+1)):
+    while (h and h[0] < (i-L+1)):
         h.popleft()
-        
-    if (h and h[-1][0]>window[i]):
-        while (h and h[-1][0]>window[i]):
-            h.pop()
+    while h and window[h[-1]]>window[i]:
+        h.pop()
     
-    h.append((window[i], i))
-    D[i] = h[0][0]
+    h.append(i)
+    D[i] = window[h[0]]
     
 print(*D)
