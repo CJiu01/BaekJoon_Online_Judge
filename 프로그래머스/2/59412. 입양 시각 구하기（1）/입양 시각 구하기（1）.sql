8c0dba46d04c0bd80af9827fc01c5757484106d4
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-02-27 17:19:35 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,27 @@
 -- 코드를 입력하세요
-SELECT hour(datetime), count(*) as count
+
+select hour(datetime), count(*)
 from ANIMAL_OUTS
 where time(datetime) between '09:00' and '19:59'
 group by hour(datetime)
 order by hour(datetime)
+
+
+
+# where time(datetime) between '09:00' and '19:59'
+
+
+
+
+
+
+
+
+
+
+
+# SELECT hour(datetime), count(*) as count
+# from ANIMAL_OUTS
+# where time(datetime) between '09:00' and '19:59'
+# group by hour(datetime)
+# order by hour(datetime)
