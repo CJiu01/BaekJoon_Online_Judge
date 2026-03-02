5a0065f8d7f5bffbf7e99877d39548d6c2147fe9
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-03-02 22:35:30 +0900

    [Silver I] Title: 회전 초밥, Time: 96 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,16 +1,39 @@
+from collections import defaultdict
+
+
 import sys
 input = sys.stdin.readline
 N, d, k, c = map(int, input().split())
 
-arr = []
-for _ in range(N):
-    arr.append(int(input()))
-    
-arr = arr+arr[:k-1]
-sushi = []
+arr = [int(input()) for _ in range(N)]
+arr += arr[:k-1]
+
+count = defaultdict(int)
+unique = 0
 res = 0
-for i in range(N):
-    cnt = len(set(arr[i:i+k]+[c]))
-    res = max(cnt,res)
+
+# 초기 윈도우 세팅
+for i in range(k):
+    if count[arr[i]] == 0:
+        unique += 1
+    count[arr[i]] += 1
     
+res = unique + (1 if count[c]==0 else 0)
+
+# 슬라이딩 시작
+for i in range(1,N):
+    # 제거할 초밥
+    left = arr[i-1]
+    count[left] -= 1
+    if count[left] == 0:
+        unique -= 1
+    
+    # 추가할 초밥
+    right = arr[i+k-1]
+    if count[right] == 0:
+        unique += 1
+    count[right] += 1
+    
+    tmp = unique + (1 if count[c]==0 else 0)
+    res = max(res, tmp)
 print(res)
