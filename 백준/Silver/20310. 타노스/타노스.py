621e523bf65f561beea3da04fbc2c48f12a90423
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-03-12 15:21:37 +0900

    [Silver III] Title: 타노스, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,38 +1,14 @@
-S = input()
-zero_cnt = 0
-one_cnt = 0
-for s in S:
-    if s=='0':
-        zero_cnt += 1
-    else:
-        one_cnt += 1
-        
-zero_cnt //= 2
-one_cnt //= 2
+S = list(input().strip())
+a = S.count('0') // 2
+b = S.count('1') // 2
 
-new = ''
 for i in range(len(S)):
-    if one_cnt == 0:
-        new += S[i:]
-        break
-    
-    if S[i]=='1':
-        one_cnt -= 1
-    else:
-        new += S[i]
-
-new = new[::-1]
-ans = ''
-
-for i in range(len(new)):
-    if zero_cnt == 0:
-        ans += new[i:]
-        break
-    
-    if new[i]=='0':
-        zero_cnt -= 1
-    else:
-        ans += new[i]
-
-ans = ans[::-1]
-print(ans)
+    if b>0 and S[i]=='1':
+        S[i] = ''
+        b -= 1
+        
+for i in range(len(S)-1,-1,-1):
+    if a>0 and S[i]=='0':
+        S[i] = ''
+        a -= 1
+print(''.join(S))
