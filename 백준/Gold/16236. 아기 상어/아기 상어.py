d44bc6fc971fc25c23a4dbe5c2234e498030608d
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-03-23 10:27:49 +0900

    [Gold III] Title: 아기 상어, Time: 56 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,4 +1,6 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 N = int(input())
 graph = [list(map(int, input().split())) for _ in range(N)]
@@ -9,18 +11,24 @@ for i in range(N):
     for j in range(N):
         if graph[i][j] == 9:
             x,y = i,j
+            graph[x][y] = 0
 
             
-def solve(x,y,size):
+def bite_fish(x,y,size):
     
     visited = [[False]*N for _ in range(N)]
-    distance = [[0]*N for _ in range(N)]
-    q = deque([(x,y)])
+    q = deque([(x,y,0)])
     visited[x][y] = True
     tmp = []
+    prev_dist = 0
     
     while q:
-        cur_x, cur_y = q.popleft()
+        cur_x, cur_y, dist = q.popleft()
+        
+        if dist != prev_dist:
+            if tmp:
+                return tmp
+            prev_dist = dist
         
         for i in range(4):
             dx = cur_x+dir[i][0]
@@ -29,25 +37,22 @@ def solve(x,y,size):
             if (0<=dx<N and 0<=dy<N) and not visited[dx][dy]:
                 if graph[dx][dy]<=size:
                     visited[dx][dy] = True
-                    distance[dx][dy] = distance[cur_x][cur_y]+1
-                    q.append([dx,dy])
-
+                    q.append([dx,dy,dist+1])
+                
                     # 먹을 수 있다면
                     if 0<graph[dx][dy]<size:
-                        tmp.append([dx,dy,distance[dx][dy]])
-                    
-    return sorted(tmp, key=lambda x: (-x[2],-x[0],-x[1]))
-
-
-graph[x][y] = 0
+                        tmp.append([dx,dy, dist+1])
+    return tmp  
+    
 fish_cnt = 0
 ans = 0
-while 1:
-    fish = solve(x,y,size)
+while True:
+    fish = bite_fish(x,y,size)
     
     if not fish:
         break
     
+    fish.sort(key=lambda x:(-x[0],-x[1]))
     nx,ny,dist = fish.pop()
     
     ans += dist
