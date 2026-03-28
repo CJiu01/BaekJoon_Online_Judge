e3529a651949e5b4a57fd87e06b4648760acdd6f
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-03-29 00:05:35 +0900

    [Silver IV] Title: 쿠키의 신체 측정, Time: 112 ms, Memory: 39224 KB -BaekjoonHub

--- 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.py
+++ 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.py
@@ -2,56 +2,32 @@ N = int(input())
 cookie = []
 for _ in range(N):
     cookie.append(list(input()))
-  
-x,y = 0,0  
-for i in range(N):
-    for j in range(N):
-        if cookie[i][j] == '*':
-            x,y = i,j
-            break
-    if y!=0:
-        break
+    
+def find_heart():
+    x,y = 0,0  
+    for i in range(N):
+        for j in range(N):
+            if cookie[i][j] == '*':
+                x,y = i,j
+                break
+        if y!=0:
+            break 
+    return x,y
 
-answer = []
-# 왼팔
-left_arm = 0
-for i in range(y-1,-1,-1):
-    if cookie[x+1][i] != '*':
-        break
-    left_arm+=1
-answer.append(left_arm)
+def find_length(x,y,dx,dy):
+    length = 0 
+    while 0 <= x < N and 0 <= y < N and cookie[x][y]=='*':
+        length+=1
+        x += dx
+        y += dy
+    return length
 
-# 오른팔
-right_arm = 0
-for i in range(y+1,N):
-    if cookie[x+1][i] != '*':
-        break
-    right_arm+=1
-answer.append(right_arm)
+x,y = find_heart()
+print(x+2, y+1)
 
-# 허리
-waist = 0
-for i in range(x+2,N):
-    if cookie[i][y] != '*':
-        break
-    waist+=1
-answer.append(waist)
-
-# 왼다리
-left_leg = 0
-for i in range(x+waist+2,N):
-    if cookie[i][y-1] != '*':
-        break
-    left_leg+=1
-answer.append(left_leg)
-
-# 오른다리
-right_leg = 0
-for i in range(x+waist+2,N):
-    if cookie[i][y+1] != '*':
-        break
-    right_leg+=1
-answer.append(right_leg)
-
-print(x+2,y+1)
-print(*answer)
+left_arm = find_length(x+1,y-1,0,-1)
+right_arm = find_length(x+1,y+1,0,1)
+waist = find_length(x+2,y,1,0)
+left_leg = find_length(x+waist+2,y-1,1,0)
+right_leg = find_length(x+waist+2,y+1,1,0)
+print(left_arm, right_arm, waist, left_leg, right_leg)
