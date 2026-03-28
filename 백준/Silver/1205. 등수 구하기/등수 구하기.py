33243cd2f82c32e570757aa4b9a4f230d305dbed
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-03-29 03:30:33 +0900

    [Silver IV] Title: 등수 구하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.py
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -1,22 +1,21 @@
 import sys
 input = sys.stdin.readline
 
+
 N, new_score, P = map(int, input().split())
 scores = []
 if N > 0:
     scores = list(map(int, input().split()))
-
-rank = 1
-same = 0
-for s in scores:
-    if s > new_score:
-        rank += 1
-    elif s == new_score:
-        same += 1
-
-if not scores and P > 0:
+    
+if N==0:
     print(1)
-elif rank <= P and rank + same <= P: 
-    print(rank)
 else:
-    print(-1)
+    if N==P and new_score <= scores[-1]:
+        print(-1)
+    else:
+        rank = 1
+        for i in scores:
+            if i>new_score:
+                rank+=1
+        print(rank)
+    
