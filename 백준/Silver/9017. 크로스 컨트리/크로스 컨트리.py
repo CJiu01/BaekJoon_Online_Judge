71fd9009bde42d25f82f8ab3652be43695b7d6ce
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-03-29 17:14:08 +0900

    [Silver III] Title: 크로스 컨트리, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -4,30 +4,17 @@ T = int(input())
 for _ in range(T):
 
     N = int(input())
-    numbers = list(map(int, input().split()))
-    teams = dict(Counter(numbers))
-
-    pass_teams = {}
-    for k,v in teams.items():
-        if v==6:
-            pass_teams[k] = [0,0,0]
-
-    pass_teams_name = list(pass_teams.keys())
-
+    l = list(map(int, input().split()))
+    counter = Counter(l)
+    dic = {}
     score = 1
+    
     for i in range(N):
-        if numbers[i] in pass_teams_name:
-            if pass_teams[numbers[i]][1]==4:
-                pass_teams[numbers[i]][2] = score 
-            elif pass_teams[numbers[i]][1]<4:
-                pass_teams[numbers[i]][0] += score
-            
-            pass_teams[numbers[i]][1] += 1
+        if counter[l[i]] == 6:
+            if l[i] in dic:
+                dic[l[i]].append(score)
+            else:
+                dic[l[i]] = [score]
             score+=1
-
-    res = []
-    for k,v in pass_teams.items():
-        res.append([v[0],v[2],k])
-        
-    res.sort(key=lambda x:(x[0],x[1]))
-    print(res[0][2])
+            
+    print(sorted(dic, key=lambda x: (sum(dic[x][0:4]), dic[x][4]))[0])
