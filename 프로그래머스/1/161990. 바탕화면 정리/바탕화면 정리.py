fa9ba82668540ea95441361092d802955292e330
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-08-24 19:21:15 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.11 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,17 +1,12 @@
+
 def solution(wallpaper):
-    answer = [100,100,-1,-1]
-    width = len(wallpaper[0])
-    length = len(wallpaper)
+    a = []
+    b = []
     
-    for i in range(length):
-        for j in range(width):
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[i])):
             if wallpaper[i][j] == "#":
-                if i<answer[0]:
-                    answer[0] = i
-                if j<answer[1]:
-                    answer[1] = j
-                if i>=answer[2]:
-                    answer[2] = i+1
-                if j>=answer[3]:
-                    answer[3] = j+1
-    return answer
+                a.append(i)
+                b.append(j)
+                
+    return [min(a), min(b), max(a)+1, max(b)+1]
