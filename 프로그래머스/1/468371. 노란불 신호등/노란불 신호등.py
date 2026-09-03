79632e71a546ec6c4c72068e9da39b3ae97a1938
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-09-03 14:46:22 +0900

    [level 1] Title: 노란불 신호등, Time: 82.75 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
@@ -1,25 +1,24 @@
 import math
 
 def solution(signals):
-    answer = 0
     
-    # lcm
-    n = 1
+    lcm = 1
     for s in signals:
-        n = math.lcm(n,sum(s))
+        a = sum(s)
+        lcm = math.lcm(lcm,a)
+    
+    for t in range(1, lcm+1):
+        is_all_yellow = True
         
-    a = [[0] for _ in range(len(signals))]
-    for i in range(len(signals)):
-        t = signals[i][1]
-        idx = signals[i][0]+1
-        d = signals[i][0] + signals[i][2]
+        for g, y, r in signals:
+            cycle_length = g+y+r
+            
+            current_pos = t % cycle_length
+            if not (g+1<= current_pos <= g+y):
+                is_all_yellow = False
+                break
+            
+        if is_all_yellow:
+            return t
         
-        while idx<=n:
-            a[i].extend([j for j in range(idx, idx+t)])
-            idx += t+d
-
-    res = set(a[0][1:])
-    for i in range(1,len(a)):
-        res = res & set(a[i][1:])
-
-    return min(res) if res else -1  
+    return -1
