10cd02f6e2f9c9ae2f406ed444a05dbf750fd0b2
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-09-05 18:58:39 +0900

    [level 2] Title: 힌트 스테이지, Time: 51.96 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.py
+++ 프로그래머스/2/468377. 힌트 스테이지/힌트 스테이지.py
@@ -1,40 +1,18 @@
-from itertools import combinations
-
-def game(buy_hint_stage, cost, hint):
-    n = len(cost)
-    hint_cnt = [0]*n
-    money = 0
-    
-    # 구매한 힌트권 몇 장인지 세팅
-    for i in buy_hint_stage:
-        h = hint[i]
-        money += h[0]
-        for j in range(1,len(h)):
-            hint_cnt[h[j]-1] += 1
-    
-        
-    # 힌트권에 따라 스테이지 해결
-    for i in range(n):
-        if len(cost[i])<=hint_cnt[i]:
-            money += cost[i][-1]
-        else:
-            money += cost[i][hint_cnt[i]]
-    
-    return money
-
 def solution(cost, hint):
-    answer = 10000000
-    n = len(hint)
-    
-    buy_hint = []
-    li = [j for j in range(n)]
-
-    for i in range(n+1):
-        buy_hint.extend(list(combinations(li, i)))
-    
-    for buy in buy_hint:
-        m = game(buy, cost, hint)
-        if m!=-1:
-            answer = min(answer, m)
-
-    return answer
+    answer = 0
+    hint.append([0,0])
+    N=len(cost)
+    def search(cost_used, now_hints, n):
+        if n==N:
+            return cost_used
+        ncost = cost_used + cost[n][min(now_hints[n], len(cost[n])-1)]
+        rt = search(ncost, now_hints, n+1)
+        hc, *hints = hint[n]
+        ncost += hc
+        for h in hints:
+            now_hints[h-1]+=1
+        rt = min(rt, search(ncost, now_hints, n+1))
+        for h in hints:
+            now_hints[h-1]-=1
+        return rt
+    return search(0, [0]*N, 0)
