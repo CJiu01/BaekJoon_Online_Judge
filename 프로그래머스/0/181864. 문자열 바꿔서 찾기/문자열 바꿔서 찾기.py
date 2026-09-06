9116ffd6873c2b4d24548acd8681dc5e24bf9f21
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-09-06 21:41:06 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -0,0 +1,12 @@
+def solution(myString, pat):
+    myString = list(myString)
+    for i in range(len(myString)):
+        if myString[i] == 'A':
+            myString[i] = 'B'
+        else:
+            myString[i] = 'A'
+    myString = ''.join(myString)
+
+    if pat in myString:
+        return 1
+    return 0
