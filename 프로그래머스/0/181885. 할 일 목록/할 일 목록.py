c7a45731631d6808cff25a1ffc55040c5a6a3af2
JIWOO CHOI <102797359+CJiu01@users.noreply.github.com>
2026-09-20 18:31:05 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,7 +1,2 @@
 def solution(todo_list, finished):
-    answer = []
-    for t,f in zip(todo_list, finished):
-        if not f:
-            answer.append(t)
-    
-    return answer
+    return [t for t,f in zip(todo_list, finished) if not f]
